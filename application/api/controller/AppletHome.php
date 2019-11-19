dbd10c0c6c20cc7f4af2713ab5ffa9a1bd03e348
FK <18310029587@163.com>
2019-11-19 16:28:45 +0800

    建立api文件夹

---
+++ application/api/controller/AppletHome.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fk
+ * Date: 2019/11/19
+ * Time: 16:25
+ */
+namespace app\api\controller;
+
+use think\Controller;
+class AppletHome extends Controller
+{
+    /**
+     * 首页
+     * $data 2019/11/19 16:26
+     */
+    public function honeIndex(){
+
+    }
+}
